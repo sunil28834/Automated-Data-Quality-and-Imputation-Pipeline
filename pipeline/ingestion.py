748daa7f3fa8e31bdeb8161e56234a8e583145e5
Sunil T R <sunilsandy87@gmail.com>
2026-04-05 17:24:16 +0530

    Delete pipeline directory

--- pipeline/ingestion.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-def ingest_data(file_path):
-    df = pd.read_csv(file_path)
-    return df
--- pipeline/quality_score.py
+++
@@ -1,9 +0,0 @@
-def quality_score(df):
-
-    total_cells = df.shape[0] * df.shape[1]
-
-    missing_cells = df.isnull().sum().sum()
-
-    score = (1 - (missing_cells / total_cells)) * 100
-
-    return round(score,2)
